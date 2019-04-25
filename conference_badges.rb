df31d9e2e15e6227f0340ecdd9d26f1da1e35378
Sean Carter <scarter0313@gmail.com>
2019-04-25 02:20:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,3 +16,8 @@ def assign_room(attendees)
   end
  end
  
+def assign_rooms(attendees)
+  attendees.each_with_index.map do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+  end
+end
