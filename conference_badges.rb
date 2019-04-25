6d5da0ec59be1d3b2eb523262dc705e6f265511d
Sean Carter <scarter0313@gmail.com>
2019-04-25 02:31:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,11 +17,11 @@ def assign_rooms(attendees)
  end
  
  def printer(attendees)
-   batch_badge_creator(attendees)each do |badge|
+   batch_badge_creator(attendees).each do |badge|
      puts badge
    end
    
-   assign_rooms(attendees)each do |assignment|
+   assign_rooms(attendees).each do |assignment|
      puts assignment
    end
  end
